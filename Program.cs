fc3ec67687258cbd0a9146f43b447033fb058350
kauamatheus-ksml <148459355+kauamatheus-ksml@users.noreply.github.com>
2025-05-10 18:52:53 +0000

    commit2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure;
+using Azure.AI.OpenAi;
+using Microsoft.Extensions.Configuration;
+using OpenAi.Chat;
+using System.Text;
+
+namespace ConsoleApp.AzureAi{
+    public class TemperatureSettings{
+        public double Temperature {get; set;} = 0.7;
+    }
+
+    public class Program{
+        private static string AzureOpenAiEndpoint = "";
+        private static string AzureOpenAiKey = "";
+        private static string DeploymentName = "gpt-35-turbo";
+
+        private static string PromptFilePath => Path.Combine(Diretory.GetCurrentDirectory(),
+        "prompt.txt");
+        
+        private static string TemperatureFilePath => Path.Cobine(Directory.GetCurrentDirectory 
+        (),"temperature.json");
+
+        public static async Task Main(string[] args)
+    }
+}
